daffe28d1d955fb5ddca2f6f81ba17ae3ac759a3
Alessio Leone <36887074+LssLn@users.noreply.github.com>
2022-04-08 23:54:06 +0200

    Update main_test.java

--- main_test.java
+++ main_test.java
@@ -12,24 +12,25 @@ public class main_test {
 		// input numbers a and b
 		System.out.print(" Given A and B, the percentage representation of the sum of these 2 numbers is returned.");
 		System.out.print("\n A: ");
-		Scanner scanner_1 = new Scanner(System.in);
- 		double a= scanner_1.nextDouble();
+		Scanner scanner1 = new Scanner(System.in);
+ 		double a= scanner1.nextDouble();
  		System.out.print(" B: ");
- 		Scanner scanner_2 = new Scanner(System.in);
- 		double b= scanner_2.nextDouble();
+ 		Scanner scanner2 = new Scanner(System.in);
+ 		double b= scanner2.nextDouble();
  		
 		
-		double perc_a = a/(a+b)  * 100;
-		double perc_b = b/(a+b) * 100;
+		double percA = a/(a+b)  * 100;
+		double percB = b/(a+b) * 100;
 		
-		String perc_b_str = convertDecimalFormat1(perc_b);
-		perc_b_str.replace(",", ".");
-		perc_b_str = perc_b_str + "%";
-		String perc_a_str = convertDecimalFormat1(perc_a);
-		perc_a_str.replace(",", ".");
-		perc_a_str = perc_a_str + "%";
+		String percBStr = convertDecimalFormat1(percB);
+		percBStr.replace(",", ".");
+		percBStr = percBStr + "%";
+		String percAStr = convertDecimalFormat1(percA);
+		percAStr.replace(",", ".");
+		percAStr = percAStr + "%";
 
-		System.out.println("\n A: "+perc_a_str + "  B:" + perc_b_str);
+		System.out.println("\n A: "+percAStr + "  B:" + percBStr);
 	}
 
 }
+
